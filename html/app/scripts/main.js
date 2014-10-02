3475716457c1837eac81284719c5a95bb51be334
Abdullo <ahidoyatov@gmail.com>
2014-10-02 16:51:49 +0500

    Zavershenie glavnoy stranitsi v2.0

---
+++ html/app/scripts/main.js
@@ -0,0 +1,26 @@
+$(function(){
+
+	$('.dropdown-menu').click(function(e) {
+        e.stopPropagation();
+    });
+
+    $("#carousel-pro").owlCarousel({
+    	items : 6,
+    	itemsDesktop : false, //5 items between 1000px and 901px
+		itemsDesktopSmall : false, // betweem 900px and 601px
+		itemsTablet: false, //2 items between 600 and 0
+		itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
+    	navigation : false
+    });
+
+	$('.select-styled').selectpicker({
+        caretHTML: '<span class="select-caret"></span>'
+    }); 
+
+    //region map start
+    var stage = new swiffy.Stage(document.getElementById('swiffycontainer'),
+          swiffyobject, {});
+      
+    stage.start();
+
+})
